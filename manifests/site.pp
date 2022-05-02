76efb719b2ac8874840f1004bb3af6f0ae9890a3
hcymor <102482810+hcymor@users.noreply.github.com>
2022-05-03 02:54:17 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,37 @@ node default{
     ensure => absent,
     }
   }
+  
+node slave1.puppet{
+  package { 'httpd':
+    ensure => installed,
+    name   => httpd,
+    }
+  
+  package { 'php':
+    ensure => installed,
+    name   => php,
+    }
+  }
+  
+  file { '/var/www/index.php':
+    source => "https://raw.githubusercontent.com/Fenikks/itacademy-devops-files/master/01-demosite-php/index.php",
+    }
+    
+    file { '/var/www/index.html':
+    ensure => absent,
+    }
+  }
+  
+node slave2.puppet{
+  package { 'httpd':
+    ensure => installed,
+    name   => httpd,
+    }
+  
+  file { '/var/www/index.html':
+    source => "https://raw.githubusercontent.com/Fenikks/itacademy-devops-files/master/01-demosite-static/index.html",
+    }
+    
+  }
+    
