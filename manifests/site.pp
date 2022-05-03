9de0e031f053f35491bb778d5f3477d298dc3ea6
hcymor <102482810+hcymor@users.noreply.github.com>
2022-05-04 01:41:51 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -73,8 +73,22 @@ node 'master.puppet'{
 
 node 'mineserver.puppet'{
 
+  package {'java-17-openjdk':
+    ensure => installed
+    }
+
+  file {'/opt/minecraft':
+    ensure => directory
+    }
+    
   file { '/opt/minecraft/server.jar':
-    source => "https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar",
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
+    }
+    
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true'
     }
 
 }
