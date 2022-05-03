1dd3a955c7d2990d003f71a445cd6f0509b123f1
hcymor <102482810+hcymor@users.noreply.github.com>
2022-05-03 13:57:48 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,27 +43,30 @@ node slave2.puppet{
     }
   }
 
-node master.puppet{
+node 'master.puppet'{
 
-  include nginx
+include nginx
 
-  package { 'nginx':
-    ensure => installed,
-    name => nginx,
-    }
-  
-  nginx::resource::server { 'slave1':
-    listen_port => 9080,
-    proxy => 'http://192.168.50.10:80',
-    }
-
-  nginx::resource::server { 'slave2':
-    listen_port => 9081,
-    proxy => 'http://192.168.50.11:80',
-    }
-  
-  service { 'nginx':
-    ensure => 'running',
-    } 
-    
+nginx::resource::server { 'static':
+  listen_port => 9080,
+  proxy => 'http://192.168.50.10:80',
   }
+
+nginx::resource::server { 'dynamic':
+  listen_port => 9081,
+  proxy => 'http://192.168.50.11:80',
+  }
+
+
+exec { 'selinux_to_permissive':
+  command     => 'setenforce 0',
+  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  user       => 'root',
+  }
+
+exec { 'reboot_nginx':
+  command     => 'systemctl restart nginx',
+  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  user => 'root',
+  }
+}
